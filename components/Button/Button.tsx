6b154cc21fd0a67684672284165ea8225813cb95
Denlich <muchalovych@gmail.com>
2023-07-25 16:43:04 +0300

    Build button
    Build button component. Download classnames library.

---
+++ components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import cn from "classnames";
+
+import { ButtonProps } from "./Button.props";
+import styles from "./Button.module.css";
+
+export const Button = ({
+  appearance,
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
+  return (
+    <button
+      className={cn(styles.button, className, {
+        [styles.primary]: appearance === "primary",
+        [styles.ghost]: appearance === "ghost",
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Htag } from "@/components";
+import { Button, Htag } from "@/components";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h1">123</Htag>
+      <Button appearance="primary" onClick={(): void => alert("I am working!")}>
+        Button
+      </Button>
+      <Button appearance="ghost">Button</Button>
     </div>
   );
 }
