d03a08957e33c47496c4ee7c173692e0d24cefac
Denlich <muchalovych@gmail.com>
2023-07-25 16:01:03 +0300

    Build htag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import React from "react";
+import { Htag } from "@/components";
+
 export default function Home(): JSX.Element {
-  return <div>123</div>;
+  return (
+    <div>
+      <Htag tag="h1">123</Htag>
+    </div>
+  );
 }
