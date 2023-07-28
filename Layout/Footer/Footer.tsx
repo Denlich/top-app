8a136d4093142e32696f33aea367af3b990a5fdc
Denlich <muchalovych@gmail.com>
2023-07-28 15:40:38 +0300

    Build Footer

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import cn from "classnames";
+import { format } from "date-fns";
 
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
+import Link from "next/link";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>
+        OwlTop © 2020 - {format(new Date(), "yyyy")} All rights reserved
+      </div>
+      <Link href="#" target="_blank">
+        User agreement
+      </Link>
+      <Link href="#" target="_blank">
+        Confidentiality policy
+      </Link>
+    </footer>
+  );
 };
